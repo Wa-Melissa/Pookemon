4dcc1ae9863deadad5fefa7c5cc47f369ba84578
Melissa Wanner <meliwan09@gmail.com>
2024-05-17 15:18:01 +0200

    mises a jour uml

--- src/Gestion_Jeu/Plateau.java
+++ src/Gestion_Jeu/Plateau.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class Plateau
 {
     private String m_carte;
-    private int m_tour = 1;
+    int m_tour = 1;
     private final String m_separation = "========================================================================";
     private String m_affichage;
 
