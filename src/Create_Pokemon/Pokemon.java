ac553f2ce95ad1220ff7de80ac414f2363288aeb
Melissa Wanner <meliwan09@gmail.com>
2024-05-24 22:57:54 +0200

    correction bug initialisation pouvoir

--- src/Create_Pokemon/Pokemon.java
+++ src/Create_Pokemon/Pokemon.java
@@ -31,19 +31,20 @@ public class Pokemon
         int definitElus = ThreadLocalRandom.current().nextInt(0,6);
         if (definitElus == 5){
             String pouvoirDonne = Initialisation.getPouvoir();
-            switch (pouvoirDonne){
-                case "Berserk" : m_pouvoir = new Berserk();
-                case "Soin simple" : m_pouvoir = new SoinSimple();break;
-                case "Soin de zone" : m_pouvoir = new SoinZone() ; break;
-                case "Kamikaze" : m_pouvoir = new Kamikaze(this); break;
-                case "Affinité Ether" : m_pouvoir = new AffiniteEther(); break;
-                case "Affinité Plomb" : m_pouvoir = new AffinitePlomb(); break;
-                case "Usurpation" : m_pouvoir = new Usurpation(this) ; break;
-                case "Extension du territoire": m_pouvoir = new ExtensionTerritoire(); break;
-                default: m_pouvoir = null;
-            }
+            if (pouvoirDonne != null){
+                switch (pouvoirDonne){
+                    case "Berserk" : m_pouvoir = new Berserk();
+                    case "Soin simple" : m_pouvoir = new SoinSimple();break;
+                    case "Soin de zone" : m_pouvoir = new SoinZone() ; break;
+                    case "Kamikaze" : m_pouvoir = new Kamikaze(this); break;
+                    case "Affinité Ether" : m_pouvoir = new AffiniteEther(); break;
+                    case "Affinité Plomb" : m_pouvoir = new AffinitePlomb(); break;
+                    case "Usurpation" : m_pouvoir = new Usurpation(this) ; break;
+                    case "Extension du territoire": m_pouvoir = new ExtensionTerritoire(); break;
+                    default: m_pouvoir = null;
+                }
+            } else {m_pouvoir = null;}
         } else {m_pouvoir = null;}
-        m_pouvoir = new Usurpation(this);
     }
 
     /**
